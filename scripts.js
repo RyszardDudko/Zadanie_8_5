bdf04b7457cc1415f647ccfbe3bee3ca4a4de3ee
790206 <ryszard.dudko@gmail.com>
2017-08-18 15:11:31 +0200

    Add new files

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var a = prompt('Podaj wartość a');
+var h = prompt('Podaj wartość b');
+var triangleArea = (a*h/2);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
